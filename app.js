9ff6c1780636d956177e6b54a5f1ccd5d94266ed
Prince Verma <9914604404p@gmail.com>
2022-02-19 15:50:29 +0530

    added login and sign up functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const submitButton = document.querySelector(".submitButton");
 const loginChooser = document.querySelector(".loginChooser");
 const signUpChooseButton = document.querySelector(".signUpChooser");
+const loader = document.querySelector(".loader");
 
 const firstBox = document.querySelector(".top-box");
 const lastBox = document.querySelector(".last-box");
@@ -11,10 +12,15 @@ const pass_in = document.querySelector("#pass_in");
 const gender_in_male = document.querySelector("#gender_in_male");
 const gender_in_female = document.querySelector("#gender_in_female");
 
+//0 = signUp 1=login
 let submitState = 0;
+
 let loginData = {};
 let signUpData = {};
-//0 = signUp 1=login
+
+//false= no  request currently going on
+//true= request currently going on
+let requestGoingOn = false;
 
 //DOM Manipulation--------------------------------------------------------------------------
 loginChooser.addEventListener("click", (e) => {
@@ -32,12 +38,37 @@ signUpChooseButton.addEventListener("click", (e) => {
   signUpChooseButton.classList.add("activeBackground");
 });
 
+function gotResponse(s = 0, response) {
+  //s=0 means bad response s=1 means good response
+
+  requestGoingOn = false; //changing request state
+  loader.classList.add("noneDisplay");
+  submitButton.classList.remove("submitButtonDownState");
+
+  if (s === 1) {
+    //good response
+    submitButton.classList.add("colorGreen");
+
+    console.log(response);
+  } else {
+    //bad response
+    console.log(response);
+    if (submitButton.classList.contains("colorGreen")) {
+      submitButton.classList.remove("colorGreen");
+    }
+  }
+}
+
 //Login or Sign up-------------------------------------------------------------------------------------------
 submitButton.addEventListener("click", (e) => {
-  submitButton.classList.add("color");
+  //Dom Manipulation
+  submitButton.classList.add("submitButtonDownState");
+  loader.classList.remove("noneDisplay");
 
   //sign up request
-  if (submitState === 0) {
+  if (submitState === 0 && !requestGoingOn) {
+    requestGoingOn = true;
+
     let gender;
     if (gender_in_male) {
       gender = "male";
@@ -58,13 +89,32 @@ submitButton.addEventListener("click", (e) => {
       data: signUpData,
     })
       .then(function (response) {
-        console.log(response);
+        gotResponse(1, response);
       })
       .catch(function (error) {
-        console.log(error);
+        gotResponse(0, error);
       });
 
     //-----------------------------------------------------------
-  } else {
+  } else if (submitState === 1 && !requestGoingOn) {
+    requestGoingOn = true;
+
+    loginData = {
+      email: email_in.value,
+      password: pass_in.value,
+    };
+    //Request Login------------------
+    axios({
+      method: "post",
+      url: "http://127.0.0.1:8000/login",
+      data: loginData,
+    })
+      .then(function (response) {
+        gotResponse(1, response);
+      })
+      .catch(function (error) {
+        gotResponse(0, error);
+      });
+    //-----------------------------
   }
 });
